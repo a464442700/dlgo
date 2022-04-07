636ee4cb4c1824671faa6218eed6d2122d113ed2
a464442700 <464442700@qq.com>
2022-04-07 14:46:42 +0800

    1

--- goboard_slow.py
+++ goboard_slow.py
@@ -46,3 +46,57 @@ class GoString():
 
     def __eq__(self, other):
         return isinstance(other, GoString) and self.color == other.color and self.stones == other.stones and  self.liberties == other.liberties
+
+#棋盘类
+class Board():
+    def __init__(self,num_rows,num_cols):
+        self.num_rows=num_rows
+        self.num_cols=num_cols
+        self._grid={}
+
+    def place_stone(self,player,point):
+        assert self.is_on_grid(point)
+        assert self._grid.get(point) is None
+        adjacent_same_color = []
+        adjacent_opposite_color = []
+        liberties = []
+        for neighbor in point.neighbors():
+            if not self.is_on_grid(neighbor):
+                continue
+            neighbor_string = self._grid.get(neighbor)
+            if neighbor_string is None:
+                liberties.append(neighbor)
+            elif neighbor_string.color == player:
+                if neighbor_string not in adjacent_same_color:
+                    adjacent_same_color.append(neighbor_string)
+                else:
+                    if neighbor_string not in adjacent_opposite_color:
+                        adjacent_opposite_color.append(neighbor_string)
+        new_string = GoString(player, [point], liberties)
+        for same_color_string in adjacent_same_color:  # <1>
+            new_string = new_string.merged_with(same_color_string)
+        for new_string_point in new_string.stones:
+            self._grid[new_string_point] = new_string
+        for other_color_string in adjacent_opposite_color:  # <2>
+            other_color_string.remove_liberty(point)
+        for other_color_string in adjacent_opposite_color:  # <3>
+            if other_color_string.num_liberties == 0:
+                self._remove_string(other_color_string)
+
+    #表示是在棋盘内
+    def is_on_grid(self,point):
+        return 1<=point.row<=self.num_rows and \
+            1<=point.col<=self.num_rows
+
+    def get(self,point):
+        string=self.__grid.get(point)
+        if string is None:
+            return None
+        return string.color
+    def get_go_string(self,point):
+        string =self._grid.get(point)
+        if string is None:
+            return None
+        return string
+
+
