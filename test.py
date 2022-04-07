e300330ce47964b990bd522312716c155be8b89c
a464442700 <464442700@qq.com>
2022-04-07 13:43:52 +0800

    1

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
-from enum import Enum
-class day(Enum):
-    day1=1
-    @property
-    def others(self):
-        return day.day1
+from collections import namedtuple
 
-print(day.others)
+class Point(namedtuple('Point', 'row col')):#namedtuple表示一个命名元组，类在括号里面，表示类的继承
+    def __init__(self):
+        assert False
+    def neighbors(self):
+        return [
+            Point(self.row - 1, self.col),
+            Point(self.row + 1, self.col),
+            Point(self.row, self.col - 1),
+            Point(self.row, self.col + 1),
+        ]
+#p=Point(1,1)
+#print(p.neighbors()[0].row)
+assert 1
+print(1^1^0)
